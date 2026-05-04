5a6a772baa761db134e87a45b8ae21b992788744
Divyanshu <divyanshuchoubey8210@gmail.com>
2026-05-04 13:09:52 +0530

    configchanges

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const String baseUrl = 'http://localhost:1425';
+  static const String baseUrl = 'https://mine-wadhwani-4.onrender.com';
   static const Duration connectTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
 }
